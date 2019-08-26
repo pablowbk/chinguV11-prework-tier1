29a3e7929ed952800596c943f2f4c41f9f83290a
pablowbk <pablo.weisbek@gmail.com>
2019-08-25 23:26:40 -0300

    add basic script to handle nav links active state

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log("hello!");
+// Sections
+const home = document.querySelector('.hero');
+const about = document.querySelector('.about');
+const services = document.querySelector('.services');
+const contact = document.querySelector('.contact');
+
+// Nav Btns
+
+const navHome = document.getElementById('navHome');
+const navAbout = document.getElementById('navAbout');
+const navServices = document.getElementById('navServices');
+const navContact = document.getElementById('navContact');
+
+
+var yOffset; // pageYOffset value
+
+function setActiveLink() {
+  var yOffset = window.pageYOffset + 80; // 80px is the fixed navbar height
+  // console.log(window.pageYOffset);
+
+  // Check if Home is visible and add 'active' class to nav btn
+  yOffset < about.offsetTop
+  ? navHome.classList.add('active')
+  : navHome.classList.remove('active')
+
+  // Check if About is visible and add 'active' class to nav btn
+  yOffset >= about.offsetTop && yOffset < services.offsetTop
+  ? navAbout.classList.add('active')
+  : navAbout.classList.remove('active')
+
+  // Check if Services is visible and add 'active' class to nav btn
+  yOffset >= services.offsetTop && yOffset < contact.offsetTop
+  ? navServices.classList.add('active')
+  : navServices.classList.remove('active')
+
+  // Check if Contact is visible and add 'active' class to nav btn
+  yOffset >= contact.offsetTop && yOffset < document.querySelector('footer').offsetTop
+  ? navContact.classList.add('active')
+  : navContact.classList.remove('active')
+}
+
+window.addEventListener('scroll', setActiveLink);
+
+
+// Nav Btns click actions
